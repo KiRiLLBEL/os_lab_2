826fa8c855d467716e8c335386559a621541536b
Kirill <kirillbelonosov@yandex.ru>
2022-09-26 16:50:00 +0300

    lab2 added cmake

--- test/run.sh
+++ test/run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 cd ..
-cd src
+cd build
 tests=$(find /home/kirill/localProjects/OSlabs/lab2/test -name "*.txt")
 make
 count=1
 for i in ${tests}
 do
     echo "-------------------------test $count-------------------------"
-    ./a.out < "$i"
+    ./main < "$i"
     echo "\n"
     let "count += 1"
 done
-make clean
